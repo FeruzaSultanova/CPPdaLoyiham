56f1a3e1e718888b0583825336ced2b604044d2f
FeruzaSultanova <145650170+FeruzaSultanova@users.noreply.github.com>
2023-09-21 12:21:52 +0500

    Birinchi joylov

---
+++ kalit2.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+using namespace std;
+// Include the string library
+#include <string>
+
+
+
+// int main() {
+//     int oy;
+//     cout << "12 oyni sonda kiriting ";
+//     cin >> oy;
+//     if (oy == 1) {
+//         cout << "Yanvar ";
+//     } else if (oy == 2){
+//         cout << "Fevral ";
+//     } else if (oy == 3) {
+//         cout << "Mart ";
+//     } else if (oy == 4) {
+//         cout << "Aprel ";
+//     } else if (oy == 5) {
+//         cout << "May ";
+//     } else if (oy == 6) {
+//         cout << "Iyun ";
+//     } else if (oy == 7) {
+//         cout << "Iyul ";
+//     } else if (oy == 8) {
+//         cout << "Avgust ";
+//     } else if (oy == 9) {
+//         cout << "Sentabr ";
+//     } else if (oy == 10) {
+//         cout << "Oktabr "; 
+//     } else if (oy == 11) {
+//         cout << "Nayabr ";
+//     }  else if (oy == 12) {
+//         cout << "Dekabr ";
+//     } else {
+//         cout << "1dan 12gacha bo`lgan oylarni birini kiriting ";
+//     }        
+//     return 0;
+// }
+
+
+// int main() {
+//     int fasl;
+//     cout << "12 oydagi faslni sonda kiriting ";
+//     cin >> fasl;
+//     if (fasl == 1) {
+//         cout << "Qish ";
+//     } else if (fasl == 2){
+//         cout << "Qish ";
+//     } else if (fasl == 3) {
+//         cout << "Bahor ";
+//     } else if (fasl == 4) {
+//         cout << "Bahor ";
+//     } else if (fasl == 5) {
+//         cout << "Bahor ";
+//     } else if (fasl == 6) {
+//         cout << "Yoz ";
+//     } else if (fasl == 7) {
+//         cout << "Yoz ";
+//     } else if (fasl == 8) {
+//         cout << "Yoz ";
+//     } else if (fasl == 9) {
+//         cout << "Kuz ";
+//     } else if (fasl == 10) {
+//         cout << "Kuz "; 
+//     } else if (fasl == 11) {
+//         cout << "Kuz ";
+//     }  else if (fasl == 12) {
+//         cout << "Qish ";
+//     } else {
+//         cout << "12 oydagi fasllardan birini kiriting ";
+//     }        
+//     return 0;
+// }
+
+// int main() {
+//     int  oy;
+//     cout << "12 oyni sonda kiriting ";
+//     cin >> oy;
+// switch (oy) {
+//     case 1:
+//         cout << "Yanvar ";
+//         break;
+//     case 2:
+//         cout << "Fevral ";
+//         break;
+//     case 3:
+//         cout << "Mart ";
+//         break;
+//     case 4:
+//         cout << "Aprel ";
+//         break;
+//     case 5:
+//         cout << "May";
+//         break;
+//     case 6:
+//         cout << "Iyun ";
+//         break;
+//     case 7:
+//         cout << "Iyul ";
+//         break;
+//     case 8:
+//         cout << "Avgust ";
+//         break;
+//     case 9:
+//         cout << "Sentabr ";
+//         break;
+//     case 10:
+//         cout << "Oktabr ";
+//         break;
+//     case 11:
+//         cout << "Nayabr ";
+//         break;
+//     case 12:
+//         cout << "Dekabr ";
+//         break;
+//    default:
+//     cout << "1dan 12gacha bo`lgan oylarni birirni kiriting ";
+// }               
+//     return 0;
+// }
+
+
+
+// int main() {
+//     int  fasl = 4;
+//     cout << "Yil fasllarni sonda kiriting: ";
+//     cin >> fasl;
+// switch (fasl) {
+//     case 1:
+//         cout << "Qish ";
+//         break;
+//     case 2:
+//         cout << "Qish ";
+//         break;
+//     case 3:
+//         cout << "Bahor ";
+//         break;
+//     case 4:
+//         cout << "Bahor ";
+//         break;
+//     case 5:
+//         cout << "Bahor";
+//         break;
+//     case 6:
+//         cout << "Yoz ";
+//         break;
+//     case 7:
+//         cout << "Yoz ";
+//         break;
+//     case 8:
+//         cout << "Yoz ";
+//         break;
+//     case 9:
+//         cout << "Kuz ";
+//         break;
+//     case 10:
+//         cout << "Kuz ";
+//         break;
+//     case 11:
+//         cout << "Kuz ";
+//         break;
+//     case 12:
+//         cout << "Qish ";
+//         break;
+//    default:
+//     cout << "Yil fasllaridan birini kiriting ";
+// }               
+//     return 0;
+// }
